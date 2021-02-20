97e0abe3973ac9d81c097d90875cb9e7ff733c20
wanderleimagri <wanderleimagri@gmail.com>
2021-02-20 19:14:21 -0300

    teste

--- src/main/kotlin/com/example/springkotlin/SpringkotlinApplication.kt
+++ src/main/kotlin/com/example/springkotlin/SpringkotlinApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BankaccountApplication
+class SpringkotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<BankaccountApplication>(*args)
+    runApplication<SpringkotlinApplication>(*args)
 }
